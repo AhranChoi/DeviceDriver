967da58dd20c1e73d7781069ef37e4c256fbb15e
ahran_.choi <carokq@gmail.com>
2024-06-03 10:23:42 +0900

    DeviceDriver - read

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,63 @@
-#include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../TDD_DeviceDriver/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class TestFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	static const int NormalData = 0xAA;
+	static const int AbnormalData = 0xBB;
+	static const int AbnormalDataAddress = 0x1;
+
+	virtual unsigned char read(long address) {
+		if (address == AbnormalDataAddress) {
+			static int i = 0;
+			i++;
+			if (i % 5 == 0) return AbnormalData;
+			return NormalData;
+		}
+		return NormalData;
+	}
+
+	virtual void write(long address, unsigned char data) {
+
+	}
+};
+
+class MockFlashMemoryDevice : public TestFlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+};
+
+class DeviceDriverFixture : public testing::Test {
+public:
+	MockFlashMemoryDevice device;
+	DeviceDriver driver{ &device };
+};
+
+TEST_F(DeviceDriverFixture, ReadSuccess) {
+	long addr = 0x0;
+
+	EXPECT_CALL(device, read, (_), ())
+		.Times(5)
+		.WillRepeatedly(testing::Return(MockFlashMemoryDevice::NormalData));
+
+	EXPECT_EQ(driver.read(addr), MockFlashMemoryDevice::NormalData);
+}
+
+TEST_F(DeviceDriverFixture, ReadException) {
+	long addr = MockFlashMemoryDevice::AbnormalDataAddress;
+
+	EXPECT_CALL(device, read, (_), ())
+		.Times(5)
+		.WillOnce(testing::Return(MockFlashMemoryDevice::NormalData))
+		.WillOnce(testing::Return(MockFlashMemoryDevice::NormalData))
+		.WillOnce(testing::Return(MockFlashMemoryDevice::NormalData))
+		.WillOnce(testing::Return(MockFlashMemoryDevice::NormalData))
+		.WillOnce(testing::Return(MockFlashMemoryDevice::AbnormalData));
+
+	try {
+		driver.read(addr);
+		FAIL();
+	}
+	catch (...) { }
 }
--- TDD_DeviceDriver/DeviceDriver.cpp
+++ TDD_DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,23 @@
+#include <exception>
 #include "DeviceDriver.h"
+
+class ReadFailException : public std::exception {
+public:
+    ReadFailException()
+        : exception("ReadFailException") { }
+};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int result = (int)(m_hardware->read(address));
+    for (int i = 1; i < 5; i++) {
+        int temp = (int)(m_hardware->read(address));
+        if (result != temp) throw ReadFailException();
+    }
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
